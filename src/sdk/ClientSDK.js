443f9c5ab12c7171314ae71abd5c56342ea72793
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2018-08-30 10:56:20 +0800

    update: import more specific object for reducing compiled size

--- src/sdk/ClientSDK.js
+++ src/sdk/ClientSDK.js
@@ -1,13 +1,13 @@
-import * as _ from "lodash";
+import uniqueId from "lodash/uniqueId";
 
 const LoginUser = {
-  id: _.uniqueId(),
+  id: uniqueId(),
   username: "John Patrick"
 };
 
 const Incidents = [
   {
-    id: _.uniqueId(),
+    id: uniqueId(),
     title: "滨湖路积水交通堵塞",
     reportTime: new Date("2018-04-01T01:29:53Z"),
     level: 1,
@@ -15,7 +15,7 @@ const Incidents = [
       "5月6日晚事发地区普降暴雨，降水量达到50毫米，加之地势低洼、排涝设施设计不科学，周边水量聚集在路面，最深处积水达到 1.1米。晚上光线较暗，过往车辆多数引擎进水熄火。同时引起周边路段交通堵塞长达2.5公里，需要及时上报市、区政府协同相关部门紧急处置。"
   },
   {
-    id: _.uniqueId(),
+    id: uniqueId(),
     title: "关山口2人争吵引围观",
     reportTime: new Date("2018-04-01T08:05:27Z"),
     level: 3,
@@ -25,20 +25,20 @@ const Incidents = [
 
 const NavigatorInfo = [
   {
-    key: _.uniqueId(),
+    key: uniqueId(),
     title: "警情管理",
     items: [
-      { key: _.uniqueId(), label: "当前警情", uri: "" },
-      { key: _.uniqueId(), label: "历史警情", uri: "" }
+      { key: uniqueId(), label: "当前警情", uri: "" },
+      { key: uniqueId(), label: "历史警情", uri: "" }
     ]
   },
   {
-    key: _.uniqueId(),
+    key: uniqueId(),
     title: "资源管理",
     items: [
-      { key: _.uniqueId(), label: "资源人", uri: "" },
-      { key: _.uniqueId(), label: "资源车", uri: "" },
-      { key: _.uniqueId(), label: "资源物资", uri: "" }
+      { key: uniqueId(), label: "资源人", uri: "" },
+      { key: uniqueId(), label: "资源车", uri: "" },
+      { key: uniqueId(), label: "资源物资", uri: "" }
     ]
   }
 ];
